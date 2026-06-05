ecd384059fca648205143dcb2b75e3103b6177ab
saifajiboye <83846088+saifajiboye@users.noreply.github.com>
2026-06-05 15:29:09 -0500

    do tidy up

--- src/app/pages/my-work/my-work.component.ts
+++ src/app/pages/my-work/my-work.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from '../../components/about-me/about-me.component';
-import { AccomplishmentsComponent } from '../../components/accomplishments/accomplishments.component';
+
 
 @Component({
   selector: 'app-my-work',
-  imports: [AboutMeComponent, AccomplishmentsComponent],
+  imports: [AboutMeComponent],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.css'
 })
